0c8cb7a7439e2378fb741dfea22b99b077fa2a4d
Brian Mauck <32370247+bmauck@users.noreply.github.com>
2024-02-22 11:48:22 -0800

    Changed order of vars Changed title Added subheader markdown

--- front_end.py
+++ front_end.py
@@ -1,34 +1,36 @@
 import streamlit as st
 import requests
 
-st.title('JIRAT')
+st.title('V3 Model Scoring')
 st.markdown('Minimum viable product for utilizing the V3 Underwriting Model')
 
 #18 V3 Model Inputs
-total_end_balance_over_outflows = st.number_input("6m Avg Ending Balance / Outflows")
-six_m_net_cash_flow = st.number_input("6m Avg")
-six_m_net_cash_flow = st.number_input("6m Average")
+st.markdown('General Business Info')
 age_of_biz = st.number_input('Adjusted age of business')
-orig_sales_vol = st.number_input('Original Sales Volatility')
-end_bal_over_outflows = st.number_input('Total ending balance/Outflows')
-avg_net_cash_flow = st.number_input('Average Net Cash Flow')
-net_cash_flow_3m = st.number_input('Net Cash Flow (3m)')
-net_cash_flow_3m = st.number_input('Net Cash flow (2m)')
-avg_net_cash_over_inflow = st.number_input('Average Cash netflow/Inflow Ratio')
-end_bal_3m = st.number_input('Ending Balance (3m)')
-end_bal_2m = st.number_input('Ending Balance (2m)')
-sales_inflow_3m = st.number_input('Sales Inflow Trend (3m/old 3m)')
-sales_inflow_2m = st.number_input('Sales Inflow Trend (2m/old 2m)')
-adj_sales_vol_3m = st.number_input('Adjusted Sales Volatility (3m)')
-adj_sales_vol_2m = st.number_input('Adjusted Sales Volatility (2m)')
-num_inquiries = st.number_input('Number of inquiries')
-num_30_plus_lines = st.number_input('Number of tradelines where customer went 30+ days past due')
-num_repaid_lines = st.number_input('Number of fully repaid tradelines (Credit Bureau)')
-num_active_lines = st.number_input('Number of active tradelines')
+
+st.markdown('Banking Data')
+total_end_balance_over_outflows = st.number_input("Total Ending Balance/Total Outflows in the Last Six Months")
+six_m_net_cash_flow = st.number_input("Average Net Cash Flow in the Last Six Months")
+six_m_net_cash_flow_ratio = st.number_input("Average Cash Netflow/Inflow Ratio in the Last Six Months")
+end_bal_25k = st.number_input('Ending Bal. >$25K?')
+net_cash_trend = st.number_input('Net Cash Flow Trend (recent 3m/oldest 3m)')
+end_bal_trend_3m = st.number_input('Ending Balance Trend (recent 3m/oldest 3m')
+end_bal_trend_2m, = st.number_input('Ending Balance Trend (recent 2m/oldest 2m')
+orig_sales_vol = st.number_input('Sales Volatility in the Last Six Months')
+sales_inflow_3m = st.number_input('Sales Inflow Trend (recent 3m/oldest 3m)')
+sales_inflow_2m = st.number_input('Sales Inflow Trend (recent 2m/oldest 2m)')
+adj_sales_vol_3m = st.number_input('Adjusted Sales Volatility (recent 3m/oldest 3m)')
+adj_sales_vol_2m = st.number_input('Adjusted Sales Volatility (recent 2m/oldest 2m)')
+
+st.markdown('Credit Bureau Data')
+num_inquiries = st.number_input('Number of Inquiries in the Last Six Months')
+num_30_plus_lines = st.number_input('Number of 30+ days past due tradelines in Last Six Months')
+num_repaid_lines = st.number_input('Number of Fully Repaid Tradelines')
+num_active_lines = st.number_input('Number of Active Tradelines')
+
 
 if st.button("Calculate"):
-    data = {
-        
+    data = {        
     }
 
     # Adjust the backend URL if you create a custom domain on Streamlit Cloud
